58182d471e5c5c0a6bbaf6b1a892f13dfbfd27cc
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 17:54:12 +0100

    update caller to instantiate a NumberRange object and pass it down to readingsOutsideRange

--- src/caller.js
+++ src/caller.js
@@ -1,12 +1,14 @@
 const station = require('./data');
+const { NumberRange } = require('./number-range');
 const readingsOutsideRange = require('./readings-outside-range');
 
 const operatingPlan = { temperatureFloor: 50, temperatureCeiling: 55 };
-
+const range = new NumberRange(operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling);
 const alerts = readingsOutsideRange(
   station,
   operatingPlan.temperatureFloor,
-  operatingPlan.temperatureCeiling
+  operatingPlan.temperatureCeiling,
+  range
 );
 
 console.log(alerts);
