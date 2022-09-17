c95a67f6066e55d77f5194f6346f2f6561d8dc7e
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 17:57:34 +0100

    start using the max field from the range obj at readingsOutsideRange

--- src/readings-outside-range/index.js
+++ src/readings-outside-range/index.js
@@ -1,5 +1,5 @@
 function readingsOutsideRange(station, min, max, range) {
-  return station.readings.filter(r => r.temp < min || r.temp > max);
+  return station.readings.filter(r => r.temp < min || r.temp > range.max);
 }
 
 module.exports = readingsOutsideRange;
--- src/readings-outside-range/index.test.js
+++ src/readings-outside-range/index.test.js
@@ -1,9 +1,11 @@
 const readingsOutsideRange = require('.');
+const { NumberRange } = require('../number-range');
 
 describe('readingsOutsideRange', () => {
   it('should return all readings that fall outside of the specified range', () => {
     const min = 10;
     const max = 30;
+    const range = new NumberRange(min, max);
     const station = {
       name: 'ZB1',
       readings: [
@@ -13,7 +15,7 @@ describe('readingsOutsideRange', () => {
       ],
     };
 
-    const results = readingsOutsideRange(station, min, max);
+    const results = readingsOutsideRange(station, min, max, range);
 
     expect(results).toHaveLength(2);
     expect(results.some(r => r.temp === 9)).toBeTruthy();
